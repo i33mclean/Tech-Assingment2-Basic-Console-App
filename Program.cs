cd62f33792eb65058b5810e9c48e5d014b2fd081
i33mclean <77039023+i33mclean@users.noreply.github.com>
2022-01-28 22:30:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assingment2_Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Ian McLean and I am excited to learn more about C#!");
+        }
+    }
+}
